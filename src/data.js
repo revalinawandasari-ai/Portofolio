5e94d15f1f788e4e0b789f837fcc787f358c2507
Revalina Wanda Sari <revalinawandasari@gmail.com>
2026-05-27 21:45:32 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,160 @@
+import HeroImage from "/assets/reva-image-pt.jpeg";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/ai.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools12 from "/assets/tools/laravel.png";
+import Tools13 from "/assets/tools/php.png";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools12,
+    nama: "Laravel",
+    ket: "PHP Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools13,
+    nama: "PHP",
+    ket: "Language",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  /*
+    id: 9,
+    gambar: Tools9,
+    nama: "Adobe Illustrator",
+    ket: "Design App",
+    dad: "900",
+  },*/
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1000",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.webp";
+import Proyek2 from "/assets/proyek/proyek2.webp";
+import Proyek3 from "/assets/proyek/proyek3.webp";
+import Proyek4 from "/assets/proyek/proyek4.webp";
+import Proyek5 from "/assets/proyek/proyek5.webp";
+import Proyek6 from "/assets/proyek/proyek6.webp";
+import Proyek7 from "/assets/proyek/proyek7.png";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek7,
+    nama: "Website Booking Tiket Pesawat Online",
+    desk: "Website bernama Garuda yang dirancang untuk memesan tiket pesawat secara online.",
+    tools: ["VSCode", "Laravel", "PHP", "HTML", "Tailwind CSS", "Javascript"],
+    dad: "200",
+  },
+  /*
+    id: 2,
+    gambar: Proyek2,
+    nama: "Company Profile",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["HTML", "CSS", "Javascript", "AOS", "Swiper", "Lightbox Gallery"],
+    dad: "300",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "Web Pernikahan 2.0",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["Vite", "ReactJS", "TailwindCSS", "AOS"],
+    dad: "400",
+  },
+  {
+    id: 4,
+    gambar: Proyek4,
+    nama: "Website Course",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["Vite", "ReactJS", "Bootstrap", "AOS"],
+    dad: "500",
+  },*/
+  {
+    id: 2,
+    gambar: Proyek5,
+    nama: "Web Portfolio",
+    desk: "Website portofolio ini dibuat untuk menampilkan profil, keahlian, dan proyek yang telah saya kerjakan sebagai mahasiswa Teknik Informatika.",
+    tools: ["VSCode", "Vite", "ReactJS", "HTML", "Tailwind CSS", "Javascript"],
+    dad: "300",
+  },
+  /*
+    id: 6,
+    gambar: Proyek6,
+    nama: "Company Profile 2.0",
+    desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
+    tools: ["NextJS", "TailwindCSS", "Framermotion"],
+    dad: "700",
+  },*/
+];
